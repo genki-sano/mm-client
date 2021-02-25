c9a270e60ebfdde92a5675f51d0dae7782d7012c
g-sano <g.sano.2441@gmail.com>
2021-02-25 22:44:18 +0900

    fix: 設定したホスト情報を適用

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.REACT_APP_URI || 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
