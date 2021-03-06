78b91eb2f2a9e15eae24d3e0bd1f46be1dcd3a2b
g-sano <g.sano.2441@gmail.com>
2021-03-06 21:44:37 +0900

    update: エラーページの修正

--- src/components/05_pages/ErrorPage.tsx
+++ src/components/05_pages/ErrorPage.tsx
@@ -1,12 +1,21 @@
-import React, { useContext } from 'react'
-import { AuthContext } from 'contexts/AuthProvider'
+import React from 'react'
+import styled from 'styled-components'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import { theme } from 'lib/theme'
+
+const Wrapper = styled(Container)`
+  padding-top: ${`${theme.spacing(3)}px`};
+  padding-bottom: ${`${theme.spacing(3)}px`};
+`
 
 export const ErrorPage: React.FC = () => {
-  const { authError } = useContext(AuthContext)
-
   return (
-    <div>
-      <p>{authError || 'エラー'}</p>
-    </div>
+    <Wrapper maxWidth="sm">
+      <Typography variant="h2" component="h1" color="primary" gutterBottom>
+        403 Error
+      </Typography>
+      <Typography>許可されていません。アクセスが拒否されました。</Typography>
+    </Wrapper>
   )
 }
