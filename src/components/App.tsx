a0ae28837516b33155f11a0a00bd024d3decaf38
g-sano <g.sano.2441@gmail.com>
2021-03-08 22:29:26 +0900

    update: ルートコンポーネントで読み込む

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { BrowserRouter, Switch } from 'react-router-dom'
 import { PrivateRoute } from 'components/PrivateRoute'
 import { IndexPage } from 'components/05_pages/IndexPage'
+import { signIn } from 'lib/store/slices/auth'
+import { setUser } from 'lib/store/slices/user'
 
 export const App: React.FC = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(signIn())
+    dispatch(setUser())
+  }, [dispatch])
+
   return (
     <BrowserRouter>
       <Switch>
--- src/index.tsx
+++ src/index.tsx
@@ -10,22 +10,19 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import { App } from 'components/App'
 import { theme } from 'lib/theme'
 import { store } from 'lib/store'
-import { signIn } from 'lib/store/slices/auth'
 
-store.dispatch(signIn()).then(() => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <StylesProvider injectFirst>
-          <MaterialThemeProvider theme={theme}>
-            <StyledThemeProvider theme={theme}>
-              <CssBaseline />
-              <App />
-            </StyledThemeProvider>
-          </MaterialThemeProvider>
-        </StylesProvider>
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById('root'),
-  )
-})
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <StylesProvider injectFirst>
+        <MaterialThemeProvider theme={theme}>
+          <StyledThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+          </StyledThemeProvider>
+        </MaterialThemeProvider>
+      </StylesProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
