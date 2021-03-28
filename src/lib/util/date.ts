2bb035b91b2df330ac4f57cc87386a1389cdea9b
g-sano <g.sano.2441@gmail.com>
2021-03-28 17:28:07 +0900

    update: デフォルトエクスポートをやめる

--- src/lib/util/date.ts
+++ src/lib/util/date.ts
@@ -1,7 +1,7 @@
-import format from 'date-fns/format'
+import { format } from 'date-fns'
 import DateUtils from '@date-io/date-fns'
 
-export default class DateFnsUtils extends DateUtils {
+export class DateFnsUtils extends DateUtils {
   getCalendarHeaderText(date: Date): string {
     return format(date, 'yyyy年M月', { locale: this.locale })
   }
